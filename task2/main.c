2ea63a03cb17b9f525dd590a039d6ea20014e091
m.shipilov <m.shipilov@g.nsu.ru>
2022-12-13 08:09:24 +0000

    task2 with OpenACC

--- task2/main.c
+++ task2/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <sys/time.h>
 
-#define ARRAY_SIZE 128
-#define FLOAT_TYPE float
+#define ARRAY_SIZE 256
 
 #define MAX(x,y) \
        ({ typeof (x) _x = (x); \
@@ -15,42 +16,67 @@
            typeof (y) _y = (y); \
          _x < _y ? _x : _y; })
 
-void
-compute() {
+void compute() {
+
 unsigned iter=0;
 float err=1.0;
 
 float tol = 0.000001;
 unsigned iter_max = 1000000;
 
-unsigned i;
-unsigned j;
+size_t i;
+size_t j;
 
 unsigned m = ARRAY_SIZE;
 unsigned n = ARRAY_SIZE;
+float arr_size = ARRAY_SIZE;
 
-FLOAT_TYPE *A = malloc(sizeof(FLOAT_TYPE) * ARRAY_SIZE * ARRAY_SIZE);
-FLOAT_TYPE *Anew = malloc(sizeof(FLOAT_TYPE) * ARRAY_SIZE * ARRAY_SIZE);
-for(int i=1;i<ARRAY_SIZE*ARRAY_SIZE;i++) {
-     A[i]=rand();
-  }
+
+float *A = malloc(sizeof(float) * ARRAY_SIZE * ARRAY_SIZE);
+float *Anew = malloc(sizeof(float) * ARRAY_SIZE * ARRAY_SIZE);
+
+//for(size_t i=0;i<ARRAY_SIZE*ARRAY_SIZE;i++) {
+//     A[i]=rand();
+//  }
+// START POINTS:
+//  20 === 30
+//  ||     ||
+//  10 === 20
+float STEP_SIZE = 10 / (arr_size-1);
+for(i=0; i<ARRAY_SIZE; i++) {
+  // upper line
+  A[i] = 20 + i*STEP_SIZE;
+  // down line
+  A[ARRAY_SIZE*(ARRAY_SIZE-1) + i] = 10 + i*STEP_SIZE;
+  // left line
+  A[i*ARRAY_SIZE] = 20 - i*STEP_SIZE;
+  // right line
+  A[i*ARRAY_SIZE + (ARRAY_SIZE-1)] = 30 - i*STEP_SIZE;
+}
+
+for(unsigned loop = 0; loop < ARRAY_SIZE*ARRAY_SIZE; loop++)
+      printf("%f ", A[loop]);
+
+#pragma acc data copy(A[:ARRAY_SIZE * ARRAY_SIZE]) create(Anew[:ARRAY_SIZE * ARRAY_SIZE])
 
 while ( err > tol && iter < iter_max ) {
   
   err = 0.0;
   iter = iter +1;
  
-  # pragma acc kernels
+  #pragma acc kernels loop independent collapse(2) reduction(max:err)
   for (j=1; j <m-1; ++j) {
+    
     for (i=1; i <n-1; ++i) {
       Anew[i+j*n] = .25 *( A[(i+1)+j*n] + A[(i-1)+j*n] + A[i+(j-1)*n] + A[i+(j+1)*n]);
       err = MAX(err, Anew[i+j*n]-A[i+j*n]);
       }
     }
-
-  // add cycle for A = Anew:
-  for (j=1; j <m; ++j) {
-    for (i=1; i <n; ++i) {
+  
+  // cycle for A = Anew:
+  #pragma acc kernels loop independent collapse(2)
+  for (j=1; j <m-1; ++j) {
+    for (i=1; i <n-1; ++i) {
       A[i+j*n] = Anew[i+j*n];
       }
     }
@@ -67,9 +93,10 @@ free(Anew);
 }
 
 int main() {
-  clock_t begin = clock();
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
   compute();
-  clock_t end = clock();
-  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("time spent=%d \n", time_spent);
+  gettimeofday(&end, NULL);
+  float time_spent = (((end.tv_sec + end.tv_usec/1000000) - (start.tv_sec + start.tv_usec/1000000)));
+  printf("time spent=%f \n", time_spent);
 }
